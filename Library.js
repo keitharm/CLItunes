155ecd6f957cb0647ac1418dc22df728c10ed99a
Keith Armstrong <keith1015@gmail.com>
2015-10-08 10:02:07 -0700

    WIP implementing Library loading songs from config paths

--- Library.js
+++ Library.js
@@ -1,33 +1,57 @@
-var fs       = require('fs');
-var path     = require('path');
-var spawn    = require('child_process').spawn;
-var _        = require('lodash');
-var convert  = require('convert-seconds');
-var mm       = require('musicmetadata');
+/*
+  Library
+  Loads in all of the songs and does the behind the scenes logic
+*/
+
+var fs        = require('fs');
+var path      = require('path');
+var spawn     = require('child_process').spawn;
+var _         = require('lodash');
+var convert   = require('convert-seconds');
+var mm        = require('musicmetadata');
+var recursive = require('recursive-readdir');
 
 var Library = function(path) {
-  // Folder that the music files are contained in
-  // Defaults to iTunes
   this.init();
+  this.songs = [];
 };
 
 Library.prototype.init = function() {
   this.loadConfig();
+  this.loadSongs();
 };
 
 // Load the config file or create it if it doesn't exist
 // Default paths for iTunes Library Folder
 Library.prototype.loadConfig = function() {
   try {
-    this.config = fs.readFileSync(process.env.HOME + "/.CLItunes.json");
+    this.config = JSON.parse(fs.readFileSync(process.env.HOME + "/.CLItunes.json"));
   } catch (e) {
     this.config = defaultConfig = {
+      extensions: ["mp3", "m4a"],  // Extensions to search for to play with afplay
       paths: [
-        process.env.HOME + "/Music/iTunes/iTunes Media/Music"
+        process.env.HOME + "/Music/iTunes/iTunes Media/Music" // Add iTunes path by default
       ]
     };
-    fs.writeFileSync(process.env.HOME + "/.CLItunes.json", JSON.stringify(this.config));
+    fs.writeFileSync(process.env.HOME + "/.CLItunes.json", JSON.stringify(this.config, null, 2));
   }
+};
+
+// Load in all of the song data and parse with mm for song info
+Library.prototype.loadSongs = function() {
+  var paths = this.getPaths();
+
+  // Go through each path and scan for songs
+  _.each(paths, function(path) {
+    recursive(path, function (err, files) {
+      console.log(files.length);
+    });
+  });
+};
+
+// Returns all of the paths of the songs in the config file
+Library.prototype.getPaths = function() {
+  return this.config.paths;
 };
 
 module.exports = Library;
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 var Library = require("./Library");
+var Player  = require('./Player');
 
 var library = new Library();
+var player  = new Player();
