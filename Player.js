2c6edd9753ecf5f878549228e7ddaeda8c0dd1b9
Keith Armstrong <keith1015@gmail.com>
2015-10-08 10:01:35 -0700

    Started implementing Player CLI View

--- Player.js
+++ Player.js
@@ -1,8 +1,32 @@
+/*
+  Player view
+  The "GUI" of the terminal that shows current song information
+*/
+
 var readline = require('readline');
 var charm    = require('charm')();
 
 var Player = function() {
+  this.init();
+};
 
+Player.prototype.init = function() {
+  charm.pipe(process.stdout);
+  //this.clearScreen();
+  //this.showCursor(false);
+};
+
+Player.prototype.clearScreen = function() {
+  charm.reset(); // Clears the screen; like /usr/bin/reset
+};
+
+// Show or hide the cursor
+Player.prototype.showCursor = function(show) {
+  if (show || show === undefined) {
+    charm.cursor(true);
+  } else {
+    charm.cursor(false);
+  }
 };
 
 
